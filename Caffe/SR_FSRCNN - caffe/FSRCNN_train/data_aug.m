f92c44a0a3cdea8a766e2423d64f25fb039d262f
Leo Zhu <leapold_zhu@yeah.net>
2018-10-12 22:00:41 +0800

    Read-FSRCNN-Caffe-code

--- Caffe/SR_FSRCNN - caffe/FSRCNN_train/data_aug.m
+++ Caffe/SR_FSRCNN - caffe/FSRCNN_train/data_aug.m
@@ -10,10 +10,12 @@ for i = 1 : length(filepaths)
     [add, im_name, type] = fileparts(filepaths(i).name);
     image = imread(fullfile(folder, filename));
     
+    % different angle 4 types and origin scale
     for angle = 0: 90 :270
         im_rot = rot90(image, angle);
         imwrite(im_rot, [savepath im_name, '_rot' num2str(angle) '.bmp']);
         
+        % different scale 4 types
         for scale = 0.6 : 0.1 :0.9
             im_down = imresize(im_rot, scale, 'bicubic');
             imwrite(im_down, [savepath im_name, '_rot' num2str(angle) '_s' num2str(scale*10) '.bmp']);
